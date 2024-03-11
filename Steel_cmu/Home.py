3b2ac82dfe58e70336c68b9ba5dde8fe4a59a817
ttwutii <161676822+ttwutii@users.noreply.github.com>
2024-03-11 18:56:14 +0700

    Update Home.py

--- Steel_cmu/Home.py
+++ Steel_cmu/Home.py
@@ -6,7 +6,7 @@ st.write('#### Titiwut Takhao :gear: ')
 st.write('##### I am Civil Engineering')
 st.write('##### at Chiang Mai University')
 st.write('Email : titiwut.tk@gmail.com')
-st.link_button("Go to LinkedIn", "linkedin.com/in/titiwut-takhao-4932a3289")
+st.link_button("Go to LinkedIn", "https://www.linkedin.com/in/titiwut-takhao-4932a3289/")
 
 st.write('')
 st.write('')
